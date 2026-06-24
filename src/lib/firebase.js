861ef2711d675721789fd3d9898c9c4c9b3cbe13
Mamadou <yagam900@gmail.com>
2026-06-24 01:32:37 +0000

    🔐 Firebase config via env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhWVIddwJ9Z0prxJLIR8pFlI6iaPQV0jg",
-  authDomain: "ecole-manager-3aad4.firebaseapp.com",
-  projectId: "ecole-manager-3aad4",
-  storageBucket: "ecole-manager-3aad4.firebasestorage.app",
-  messagingSenderId: "921931976798",
-  appId: "1:921931976798:web:aa252717bc6a43a026e4cf"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
